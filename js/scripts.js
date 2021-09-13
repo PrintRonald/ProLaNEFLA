a4c817fed6ad8ba82879cbfc8339a6324d957607
PrintRonald <madariaga492@gmail.com>
2021-09-13 17:31:12 -0300

    Versión 14
    Se agregan a las páginas de películas un deslizamiento tal que se pueda tener una mejor visualización del contenido.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,34 @@ $(document).ready(function(){
   $("button").click(function(){
     $("#off").hide();
   });
+
+  $("#flip").mouseenter(function(){
+    $("#panel").slideToggle("slow");
+  })
+
+  $("#flip1").mouseenter(function(){
+    $("#panel1").slideToggle("slow");
+  })
+
+  $("#flip2").mouseenter(function(){
+    $("#panel2").slideToggle("slow");
+  })
+
+  $("#flip3").mouseenter(function(){
+    $("#panel3").slideToggle("slow");
+  })
+
+  $("#flip4").mouseenter(function(){
+    $("#panel4").slideToggle("slow");
+  })
+
+  $("#flip5").mouseenter(function(){
+    $("#panel5").slideToggle("slow");
+  })
+
+  $("#flip6").mouseenter(function(){
+    $("#panel6").slideToggle("slow");
+  })
 });
 
 var myVar = setInterval(myTimer, 1000);
